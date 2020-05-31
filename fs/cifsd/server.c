a62af8360ab53f9014bbca9def3ae2ddee1c4695
Namjae Jeon <namjae.jeon@samsung.com>
2020-06-01 08:33:46 +0900

    cifsd: verify and set signature for compound request/response
    When mounting with signing(sec=ntlmv2i), The following error messages
    are coming.
    
    [ 2045.267558] CIFS: Attempting to mount //10.88.103.87/homes2
    [ 2045.278424] CIFS VFS: sign fail cmd 0x3 message id 0x3
    [ 2045.278429] CIFS VFS: \10.88.103.87 SMB signature verification
    returned error = -13
    [ 2045.278916] CIFS VFS: sign fail cmd 0x3 message id 0x4
    [ 2045.278920] CIFS VFS: \10.88.103.87 SMB signature verification
    returned error = -13
    [ 2045.279266] CIFS VFS: sign fail cmd 0x5 message id 0x5
    
    The problem is that cifsd doesn't set and check signature as per compound
    request. This patch make cifsd correctly working signing verification and
    setting with compounding and fix the file deletion issue with windows 10
    client.
    
    Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifsd/server.c
+++ fs/cifsd/server.c
@@ -136,8 +136,7 @@ andx_again:
 		return TCP_HANDLER_CONTINUE;
 	}
 
-	if (work->sess && conn->ops->is_sign_req &&
-		conn->ops->is_sign_req(work, command)) {
+	if (conn->ops->is_sign_req(work, command)) {
 		ret = conn->ops->check_sign_req(work);
 		if (!ret) {
 			conn->ops->set_rsp_status(work, STATUS_ACCESS_DENIED);
@@ -224,6 +223,11 @@ static void __handle_ksmbd_work(struct ksmbd_work *work,
 				goto send;
 			}
 		}
+
+		if ((work->sess && work->sess->sign) ||
+		     smb3_11_final_sess_setup_resp(work) ||
+		     conn->ops->is_sign_req(work, command))
+			conn->ops->set_sign_rsp(work);
 	} while (is_chained_smb2_message(work));
 
 	if (work->send_no_response)
@@ -238,11 +242,7 @@ send:
 			conn->ops->set_rsp_status(work, STATUS_DATA_ERROR);
 			goto send;
 		}
-	} else if (work->sess && (work->sess->sign ||
-		smb3_final_sess_setup_resp(work) ||
-		(conn->ops->is_sign_req &&
-		conn->ops->is_sign_req(work, command))))
-		conn->ops->set_sign_rsp(work);
+	}
 
 	ksmbd_conn_write(work);
 }
