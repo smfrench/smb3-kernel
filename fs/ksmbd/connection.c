420d67397970bd911d89946e8a15f3bd2aa05381
Ralph Boehme <slow@samba.org>
2021-09-28 05:40:30 +0200

    SQUASH: at this layer we should only against the MAX_STREAM_PROT_LEN case
    Checking each SMB2 PDU has at least sizeof(struct smb2_pdu) bytes is already
    done (and must be done) in is_chained_smb2_message(). There it will catch
    non-compounded and compounded SMB2 PDUs, here it would only catch non-compounded
    ones.

--- fs/ksmbd/connection.c
+++ fs/ksmbd/connection.c
@@ -300,8 +300,7 @@ int ksmbd_conn_handler_loop(void *p)
 		 * Check if pdu size is valid (min : smb header size,
 		 * max : 0x00FFFFFF).
 		 */
-		if (pdu_size < __SMB2_HEADER_STRUCTURE_SIZE ||
-		    pdu_size > MAX_STREAM_PROT_LEN) {
+		if (pdu_size > MAX_STREAM_PROT_LEN) {
 			continue;
 		}
 
