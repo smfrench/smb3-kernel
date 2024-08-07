f5b5ed1f50fc46d2cfb3f074a367f49571ca4087
Steve French <stfrench@microsoft.com>
2024-08-07 00:07:28 -0500

    cifs: reconnect only the connection and not smb session where possible
    Commit 183eea2ee5ba upstream.
    
    With the new per-channel bitmask for reconnect, we have an option to
    reconnect the tcp session associated with the channel without reconnecting
    the smb session. i.e. if there are still channels to operate on, we can
    continue to use the smb session and tcon.
    
    However, there are cases where it makes sense to reconnect the smb session
    even when there are active channels underneath. For example for
    SMB session expiry.
    
    With this patch, we'll have an option to do either, and use the correct
    option for specific cases.
    
    Signed-off-by: Shyam Prasad N <sprasad@microsoft.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/smb2ops.c
+++ fs/cifs/smb2ops.c
@@ -4832,7 +4832,7 @@ handle_read_data(struct TCP_Server_Info *server, struct mid_q_entry *mid,
 	if (server->ops->is_session_expired &&
 	    server->ops->is_session_expired(buf)) {
 		if (!is_offloaded)
-			cifs_reconnect(server);
+			cifs_reconnect(server, true);
 		return -1;
 	}
 
@@ -5247,13 +5247,13 @@ smb3_receive_transform(struct TCP_Server_Info *server,
 						sizeof(struct smb2_sync_hdr)) {
 		cifs_server_dbg(VFS, "Transform message is too small (%u)\n",
 			 pdu_length);
-		cifs_reconnect(server);
+		cifs_reconnect(server, true);
 		return -ECONNABORTED;
 	}
 
 	if (pdu_length < orig_len + sizeof(struct smb2_transform_hdr)) {
 		cifs_server_dbg(VFS, "Transform message is broken\n");
-		cifs_reconnect(server);
+		cifs_reconnect(server, true);
 		return -ECONNABORTED;
 	}
 
