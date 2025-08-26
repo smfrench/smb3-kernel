60aeb3fbd965f2e43548d32fe7cf8aa62571f095
Stefan Metzmacher <metze@samba.org>
2025-08-26 15:01:28 +0200

    smb: smbdirect: introduce smbdirect_connection.c with the first helper function
    This is a very basic start in order to introduce smbdirect_connection.c
    with common functions, which will be shared by client and server.
    
    Over time smbdirect_connection.c will get more and more functions
    which will be included in fs/smb/client/smbdirect.c and
    fs/smb/server/transport_rdma.c via
    fs/smb/common/smbdirect/smbdirect_all_c_files.c
    in order to allow tiny steps in the direction of moving to
    a few exported functions from an smbdirect.ko.
    That's why __maybe_unused is added for now it will
    be removed at the end of the road to common code.
    
    Note the Copyright (C) 2017, Microsoft Corporation is added
    as a lot of functions from fs/smb/client/smbdirect.c will
    be moved into this file soon and I don't want to forget
    about adding it.
    
    Cc: Steve French <smfrench@gmail.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: Long Li <longli@microsoft.com>
    Cc: Namjae Jeon <linkinjeon@kernel.org>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>

---
+++ fs/smb/common/smbdirect/smbdirect_connection.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ *   Copyright (C) 2017, Microsoft Corporation.
+ *   Copyright (c) 2025, Stefan Metzmacher
+ */
+
+#include "smbdirect_internal.h"
+
+__maybe_unused /* this is temporary while this file is included in orders */
+static void smbdirect_socket_prepare_create(struct smbdirect_socket *sc,
+					    const struct smbdirect_socket_parameters *sp,
+					    struct workqueue_struct *workqueue)
+{
+	smbdirect_socket_init(sc);
+
+	/*
+	 * Make a copy of the callers parameters
+	 * from here we only work on the copy
+	 */
+	sc->parameters = *sp;
+
+	/*
+	 * Remember the callers workqueue
+	 */
+	sc->workqueue = workqueue;
+}
