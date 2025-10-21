830470082ddaf885746306bbfddcae16cff872c5
Stefan Metzmacher <metze@samba.org>
2025-10-21 19:42:17 +0200

    smb: smbdirect: introduce smbdirect_connection_is_connected()
    This is a simple way to check is the connection is still ok
    without the need to know internals of struct smbdirect_socket.
    
    Cc: Steve French <smfrench@gmail.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: Long Li <longli@microsoft.com>
    Cc: Namjae Jeon <linkinjeon@kernel.org>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/common/smbdirect/smbdirect_connection.c
+++ fs/smb/common/smbdirect/smbdirect_connection.c
@@ -951,6 +951,14 @@ static void smbdirect_connection_disconnect_work(struct work_struct *work)
 }
 
 __maybe_unused /* this is temporary while this file is included in orders */
+static bool smbdirect_connection_is_connected(struct smbdirect_socket *sc)
+{
+	if (unlikely(!sc || sc->first_error || sc->status != SMBDIRECT_SOCKET_CONNECTED))
+		return false;
+	return true;
+}
+
+__maybe_unused /* this is temporary while this file is included in orders */
 static int smbdirect_connection_wait_for_connected(struct smbdirect_socket *sc)
 {
 	struct smbdirect_socket_parameters *sp = &sc->parameters;
