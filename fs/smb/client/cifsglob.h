7ca735dd05b6c3933f3aaeb066faa2f998c8cc27
Paulo Alcantara <pc@manguebit.org>
2026-03-18 21:43:51 -0300

    smb: client: fix generic/694 due to wrong ->i_blocks
    When updating ->i_size, make sure to always update ->i_blocks as well
    until we query new allocation size from the server.
    
    generic/694 was failing because smb3_simple_falloc() was missing the
    update of ->i_blocks after calling cifs_setsize().  So, fix this by
    updating ->i_blocks directly in cifs_setsize(), so all places that
    call it doesn't need to worry about updating ->i_blocks later.
    
    Reported-by: Shyam Prasad N <sprasad@microsoft.com>
    Closes: https://lore.kernel.org/r/CANT5p=rqgRwaADB=b_PhJkqXjtfq3SFv41SSTXSVEHnuh871pA@mail.gmail.com
    Signed-off-by: Paulo Alcantara (Red Hat) <pc@manguebit.org>
    Cc: David Howells <dhowells@redhat.com>
    Cc: linux-cifs@vger.kernel.org
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/client/cifsglob.h
+++ fs/smb/client/cifsglob.h
@@ -2386,4 +2386,10 @@ static inline int cifs_open_create_options(unsigned int oflags, int opts)
 	return opts;
 }
 
+/*
+ * The number of blocks is not related to (i_size / i_blksize), but instead
+ * 512 byte (2**9) size is required for calculating num blocks.
+ */
+#define CIFS_INO_BLOCKS(size) ((512 - 1 + (size)) >> 9)
+
 #endif	/* _CIFS_GLOB_H */
